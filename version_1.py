394527d583bb9f160ee8a94b7624b3045719c86c
hmehner <127617819+hmehner@users.noreply.github.com>
2025-09-25 19:01:38 +0200

    Richtiges Package für DHT11 Sensor

--- version_1.py
+++ version_1.py
@@ -1,8 +1,8 @@
 import time
 import board
-import adafruit_dht
 import busio
 from adafruit_ht16k33 import segments
+import dht11
 
 # Initialisierung des 7-Segment-Displays und des DHT11-Sensors
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -10,22 +10,24 @@ display = segments.Seg7x4(i2c)
 display.fill(0)
 
 # Initialisierung des DHT11-Sensors am Pin D4
-sensor = adafruit_dht.DHT11(board.D4)
+sensor = dht11.DHT11(pin = 4)
 
 # Endlosschleife zur kontinuierlichen Messung und Anzeige
 while True:
     try:
-        temperature_C = sensor.temperature
+        temperature_C = sensor.read()
 
         # Anzeige der Temperatur 
         display.fill(0)
-        display.print ("{:2.1f}".format(temperature_C))
-        time.sleep (3.0)
+        display.print ("%-3.1fC" % temperature_C.temperature)
+        print ("%-3.1f C" % temperature_C.temperature)
+        time.sleep (10.0)
 
     except RuntimeError as error:
         print (error.args[0])
-        time.sleep (2.0)
+        time.sleep (5.0)
         continue
 
     except Exception as error:
+        print(temperature_C)
         raise error
