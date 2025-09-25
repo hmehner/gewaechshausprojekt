ab1e76f5ec84f9e60d08b11f8a2a639a24ed7bbe
kai <sircash1104@gmail.com>
2025-09-25 10:52:47 +0200

    erste aufgabe dauerhafte temp. messung

--- version_1.py
+++ version_1.py
@@ -1,15 +1,28 @@
 import time
 import board
 import adafruit_dht
+import busio
+from adafruit_ht16k33 import segments
 
+# Initialisierung des 7-Segment-Displays und des DHT11-Sensors
+i2c = busio.I2C(board.SCL, board.SDA)
+display = segments.Seg7x4(i2c)
+display.fill(0)
+
+# Initialisierung des DHT11-Sensors am Pin D4
 sensor = adafruit_dht.DHT11(board.D4)
 
+# Endlosschleife zur kontinuierlichen Messung und Anzeige
 while True:
     try:
         temperature_C = sensor.temperature
         humidity = sensor.humidity
-        print ("Temparatur: {:.1f} °C Luchtfeuchte: {}%".format(temperature_C, humidity))
-    
+
+        # Anzeige der Temperatur 
+        display.fill(0)
+        display.print ("{:2.1f}".format(temperature_C))
+        time.sleep (3.0)
+
     except RuntimeError as error:
         print (error.args[0])
         time.sleep (2.0)
@@ -17,5 +30,3 @@ while True:
 
     except Exception as error:
         raise error
-    
-    time.sleep (3.0)
