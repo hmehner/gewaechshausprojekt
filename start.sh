ee72c632a34ff527ced8071d40e9c7ca0d05a73d
hmehner <127617819+hmehner@users.noreply.github.com>
2026-03-20 09:04:03 +0100

    Added Setup script for preconfiguration

--- start.sh
+++ start.sh
@@ -4,4 +4,18 @@ if [ ! -f ./.env ]; then
     cp .env.template .env
 fi
 
+echo "Test if Time Server exists..."
+
+IS_CONFIGURED=$(sudo grep -E "^NTP=${TIMESERVER}" /etc/systemd/timesyncd.conf)
+
+if [[ ! -z "$IS_CONFIGURED" ]]; then
+    echo "Timeserver already configured..."
+else
+    echo "Configuring Time Server..."
+    sudo sed -i -E "s/^#*NTP=.*/NTP=${TIMESERVER}/g" /etc/systemd/timesyncd.conf
+    sudo systemctl restart systemd-timesyncd
+fi
+
+echo "Starting main programm..."
+
 python3 version_5.py
